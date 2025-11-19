ce7454cdc47e0aefb2586562a60c08ae7a0fdc93
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-11-19 14:48:31 -0500

    Se agregaron configuraciones adicionales a las configs de la app para evitar bloqueos de envios de email por parte de render.com o google

--- back-end/src/main/java/com/example/ImageHub/config/MailConfig.java
+++ back-end/src/main/java/com/example/ImageHub/config/MailConfig.java
@@ -22,7 +22,7 @@ public class MailConfig {
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+        mailSender.setPort(465);
         mailSender.setUsername(mail);
         mailSender.setPassword(password);
 
